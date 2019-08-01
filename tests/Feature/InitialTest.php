f42fa7322c924e67bb6e0af11fcf7cc5430cf654
sam <sam.nyx@live.com>
2019-08-01 23:03:31 +0530

    Basic Test

--- tests/Feature/InitialTest.php
+++ tests/Feature/InitialTest.php
@@ -4,7 +4,7 @@
 namespace sam0hack\Distributor\Tests\Feature;
 
 
-use sam0hack\Distributor\DistributorLevel;
+
 use sam0hack\Distributor\Tests;
 
 class InitialTest extends Tests\TestCase
@@ -12,7 +12,7 @@ class InitialTest extends Tests\TestCase
 
     public function test_test(){
 
-        //DistributorLevel::levelMaker(60, 'Qa7styAsRA');
+       $this->assertTrue(true);
 
     }
 }
